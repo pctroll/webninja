83b7099bc231efffae2b695ddc0c1767463d1186
Christian Chomiak <christianchomiak@gmail.com>
2013-07-29 22:51:13 -0430

    Prototype almost ready
    Game Over screen, points, lives and splash disappearance

--- javascripts/scene_gameover.js
+++ javascripts/scene_gameover.js
@@ -1,12 +1,12 @@
-function zeroFill ( number, width ) {
+/*function zeroFill ( number, width ) {
     width -= number.toString().length;
     if ( width > 0 ) {
         return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
     }
     return number + ""; // always return a string
-}
+}*/
 
-var MainScene = Class.create(Scene, {
+var GameOverScene = Class.create(Scene, {
     initialize: function (gameObj) {
         Scene.apply(this);
         this.m_GameObj = gameObj;
@@ -16,16 +16,20 @@ var MainScene = Class.create(Scene, {
         this.m_Lives = 3;
         this.m_LblScore = new Label("");
         this.m_LblScore.font = "42px Caesar Dressing";
-        this.m_LblScore.text = zeroFill(this.m_Score,3);
+        this.m_LblScore.text = zeroFill(this.m_Score, 3);
         this.m_LblScore.x = 57;
         this.m_LblScore.color = "#ffde00";
         this.m_World = new PhysicsWorld(0.0, 9.8);
         var background = new Sprite(760,570);
+        var backgroundOverlay = new Sprite(760,570);
         var scoreIcon = new Sprite(50,50);
         scoreIcon.x = 5;
         scoreIcon.y = 5;
         background.image = this.m_GameObj.assets[g_ImgBackgroundGameplay];
+        backgroundOverlay.image = this.m_GameObj.assets[g_ImgBackgroundGameOver];
+        backgroundOverlay.opacity = 0.75;
         this.addChild(background);
+        this.addChild(backgroundOverlay);
         scoreIcon.image = this.m_GameObj.assets[g_ImgScore];
         this.addChild(scoreIcon);
         for (i = 0; i < 3; i++) {
@@ -40,34 +44,46 @@ var MainScene = Class.create(Scene, {
             });
         }
         this.addChild(this.m_LblScore);
+
+        this.m_LblPlay = new Label("");
+        this.m_LblPlay.font = "42px Caesar Dressing";
+        this.m_LblPlay.text = "Play again?";
+        this.m_LblPlay.x = this.m_GameObj.width / 2 - 100;
+        this.m_LblPlay.y = this.m_GameObj.height / 2 - 25;
+        this.m_LblPlay.color = "#ffde00";
+        this.addChild(this.m_LblPlay);
+
+        this.playIcon = new Sprite(100,100);
+        this.playIcon.x = this.m_GameObj.width / 2 - 50;
+        this.playIcon.y = this.m_GameObj.height / 2 + 50;
+        this.playIcon.image = this.m_GameObj.assets[g_ImgFruits];
+        this.playIcon.frame = 0;
+        this.addChild(this.playIcon);
+        
+        this.m_WantToPlay = false;
+
+        this.playIcon.addEventListener(Event.TOUCH_START, function (e) {
+            var x = this.x;
+            var y = this.y;
+            var splash = new Splash(this.scene.m_GameObj,x,y);
+            this.scene.addChild(splash);
+            this.scene.m_WantToPlay = true;
+            this.scene.removeChild(this.scene.playIcon);
+        });
+
+        this.lifeTimer = 0;
+        this.lifeTimerLimit = this.m_GameObj.fps * 10;
+
         this.addEventListener(Event.ENTER_FRAME, this.update);
     },
     update: function (evt) {
-
-        if (this.m_Lives == 0)
+        if (this.m_WantToPlay)
         {
-            //GameOver
-            console.log("GAME OVER");
-            //var scene = new MainScene(game);
-            //this.m_GameObj.popScene(this);
-            //this.m_GameObj.replaceScene(new GameOverScene(this.m_GameObj));
-            goToScene("over", this.m_GameObj);
-        }
-        else
-        {
-            if (this.m_GameObj.frame % this.m_GameObj.fps == 0) {
-                var fruit = new Fruit(this.m_GameObj);
-                this.addChild(fruit);
-            }
-            this.m_World.step(this.m_GameObj.fps);
-
-            //We avoid the calling of zeroFill if the score hasn't changed
-            if (this.m_Score != this.m_TempScore)
+            this.lifeTimer += evt.elapsed;
+            if (this.lifeTimer > this.lifeTimerLimit)
             {
-                this.m_Score = this.m_TempScore;
-                this.m_LblScore.text = zeroFill(this.m_Score,3);
+                goToScene("main", this.scene.m_GameObj);
             }
         }
-
     }
 });
--- javascripts/splash.js
+++ javascripts/splash.js
@@ -1,10 +1,22 @@
 var Splash = Class.create(Sprite, {
-	initialize: function (gameObj, x, y) {
-		Sprite.call(this, 233, 206);
-		this.m_GameObj = gameObj;
-		this.touchEnabled = false;
-		this.image = this.m_GameObj.assets[g_ImgSplash];
-		this.x = x - this.width/2;
-		this.y = y - this.height/4;		
-	}
+    initialize: function (gameObj, x, y) {
+        Sprite.call(this, 233, 206);
+        this.m_GameObj = gameObj;
+        this.touchEnabled = false;
+        this.image = this.m_GameObj.assets[g_ImgSplash];
+        this.x = x - this.width/2;
+        this.y = y - this.height/4;
+        this.addEventListener(Event.ENTER_FRAME, this.update);
+        this.lifeTimer = 0;
+        this.lifeTimerLimit = this.m_GameObj.fps * 10;
+    },
+
+    update: function (evt) {
+        this.lifeTimer += evt.elapsed;
+        if (this.lifeTimer > this.lifeTimerLimit)
+        {
+            this.scene.removeChild(this);
+        }
+
+    }
 });
